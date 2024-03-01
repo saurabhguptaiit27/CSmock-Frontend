7c2fe83ea94d90d4a82af2951254e0dc1f28e446
saurabhguptaiit27 <saurabhguptaiit27@gmail.com>
2024-03-01 23:36:00 +0530

    main jsx updated for react routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,63 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Layout from './Layout.jsx'
+import Home from './components/Home/Home.jsx'
+import About from './components/About/About.jsx'
+import Faqs from './components/FAQs/Faqs.jsx'
+import Register from './components/Register/Register.jsx'
+import Login from './components/Login/Login.jsx'
+import IntExperiences from './components/IntExperiences/IntExperiences.jsx'
+import OurExperts from './components/OurExperts/OurExperts.jsx'
+
+
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element:<Layout/>,
+    children:[
+      {
+        path:"",
+        element:<Home/>
+      },
+      {
+        path:"about",
+        element:<About/>
+      },
+      {
+        path:"ourexperts",
+        element:<OurExperts/>
+      },
+      {
+        path:"interviewexperiences",
+        element:<IntExperiences/>
+      },
+      {
+        path:"FAQs",
+        element:<Faqs/>
+      },
+      {
+        path:"Register",
+        element:<Register/>
+      },
+      {
+        path:"Login/",
+        element:<Login/>
+      },
+      {
+        path:"Login/Register",
+        element:<Register/>
+      }
+    ]
+  }
+])
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router = {router}/>
   </React.StrictMode>,
 )
