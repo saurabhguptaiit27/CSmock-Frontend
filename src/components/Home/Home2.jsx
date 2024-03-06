f83b38309096de5928285527b75e58fa6c6f8aa3
saurabhguptaiit27 <saurabhguptaiit27@gmail.com>
2024-03-06 16:54:39 +0530

    Home2 component changed

--- src/components/Home/Home2.jsx
+++ src/components/Home/Home2.jsx
@@ -2,112 +2,153 @@ import React from "react";
 
 const Home2 = () => {
   return (
-    <section className="bg-gray-900 text-white">
-      <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8 lg:py-12">
-        <div className="mx-auto max-w-lg text-center">
-          <h2 className="text-3xl font-bold sm:text-4xl text-yellow-300">
-            What makes us special
-          </h2>
+    <section class="bg-gradient-to-r from-gray-950 via-gray-900 to-gray-700 w-full h-full">
+      <div class="container px-6 py-5 mx-auto">
+        <h1 class="text-2xl font-semibold text-green-400 capitalize lg:text-3xl ">
+          Why To Choose Us <span className="text-yellow-400">?</span>
+        </h1>
 
-          <p className="mt-4 text-gray-300">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat
-            dolores iure fugit totam iste obcaecati. Consequatur ipsa quod ipsum
-            sequi culpa delectus, cumque id tenetur quibusdam, quos fuga minima.
-          </p>
+        <div class="mt-2 -mb-10">
+          <span class="inline-block w-40 h-1 bg-blue-500 rounded-full"></span>
+          <span class="inline-block w-10 h-1 ml-1 bg-blue-500 rounded-full"></span>
+          <span class="inline-block w-8 h-1 ml-1 bg-blue-500 rounded-full"></span>
+          <span class="inline-block w-3 h-1 ml-1 bg-blue-500 rounded-full"></span>
+          <span class="inline-block w-1 h-1 ml-1 bg-blue-500 rounded-full"></span>
+          <span class="inline-block w-1 h-1 ml-1 bg-blue-500 rounded-full"></span>
+          <span class="inline-block w-1 h-1 ml-1 bg-blue-500 rounded-full"></span>
         </div>
 
-        <div className="mt-8 grid grid-cols-1 gap-8 md:mt-16 md:grid-cols-2 md:gap-12 lg:grid-cols-3">
-          <div className="flex items-start gap-4">
-            <span className="shrink-0 rounded-lg bg-gray-800 p-4 text-green-400">
-              <svg
-                className="h-5 w-5"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path d="M12 14l9-5-9-5-9 5 9 5z"></path>
-                <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"></path>
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
-                ></path>
-              </svg>
-            </span>
+        <div class="mt-10 mb-10 xl:mt-12 lg:flex lg:items-center">
+          <div class="grid w-full grid-cols-1 gap-8 lg:w-1/2 xl:gap-16 md:grid-cols-2">
+            <div class="space-y-3 p-3 shadow-xl hover:shadow-green-500/10">
+              <span class="inline-block p-3 text-blue-700 bg-blue-100 rounded-2xl">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="w-6 h-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"
+                  />
+                </svg>
+              </span>
 
-            <div>
-              <h2 className="text-lg font-bold">Lorem, ipsum dolor.</h2>
+              <h1 class="text-xl font-semibold text-yellow-400 capitalize ">
+                New Components
+              </h1>
 
-              <p className="mt-1 text-sm text-gray-300">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
-                cumque tempore est ab possimus quisquam reiciendis tempora
-                animi! Quaerat, saepe?
+              <p class="text-gray-400 text-justify">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Provident ab nulla quod dignissimos vel non corrupti doloribus
+                voluptatum eveniet
+              </p>
+            </div>
+
+            <div class="space-y-3 p-3 shadow-xl hover:shadow-green-500/10">
+              <span class="inline-block p-3 text-blue-700 bg-blue-100 rounded-2xl ">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="w-6 h-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M17.657 18.657A8 8 0 016.343 7.343S7 9 9 10c0-2 .5-5 2.986-7C14 5 16.09 5.777 17.656 7.343A7.975 7.975 0 0120 13a7.975 7.975 0 01-2.343 5.657z"
+                  />
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M9.879 16.121A3 3 0 1012.015 11L11 14H9c0 .768.293 1.536.879 2.121z"
+                  />
+                </svg>
+              </span>
+
+              <h1 class="text-xl font-semibold text-yellow-400 capitalize dark:text-white">
+                elegant Dark Mode
+              </h1>
+
+              <p class="text-gray-400 text-justify">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Provident ab nulla quod dignissimos vel non corrupti doloribus
+                voluptatum eveniet
+              </p>
+            </div>
+
+            <div class="space-y-3 p-3 shadow-xl hover:shadow-green-500/10">
+              <span class="inline-block p-3 text-blue-700 bg-blue-100 rounded-2xl ">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="w-6 h-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M11 4a2 2 0 114 0v1a1 1 0 001 1h3a1 1 0 011 1v3a1 1 0 01-1 1h-1a2 2 0 100 4h1a1 1 0 011 1v3a1 1 0 01-1 1h-3a1 1 0 01-1-1v-1a2 2 0 10-4 0v1a1 1 0 01-1 1H7a1 1 0 01-1-1v-3a1 1 0 00-1-1H4a2 2 0 110-4h1a1 1 0 001-1V7a1 1 0 011-1h3a1 1 0 001-1V4z"
+                  />
+                </svg>
+              </span>
+
+              <h1 class="text-xl font-semibold text-yellow-400 capitalize ">
+                Easy to customiztions
+              </h1>
+
+              <p class="text-gray-400 text-justify">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Provident ab nulla quod dignissimos vel non corrupti doloribus
+                voluptatum eveniet
+              </p>
+            </div>
+
+            <div class="space-y-3 p-3 shadow-xl hover:shadow-green-500/10">
+              <span class="inline-block p-3 text-blue-700 bg-blue-100 rounded-2xl ">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="w-6 h-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M5 3v4M3 5h4M6 17v4m-2-2h4m5-16l2.286 6.857L21 12l-5.714 2.143L13 21l-2.286-6.857L5 12l5.714-2.143L13 3z"
+                  />
+                </svg>
+              </span>
+
+              <h1 class="text-xl font-semibold text-yellow-400 capitalize ">
+                Simple & clean designs
+              </h1>
+
+              <p class="text-gray-400 text-justify">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Provident ab nulla quod dignissimos vel non corrupti doloribus
+                voluptatum eveniet
               </p>
             </div>
           </div>
 
-          <div className="flex items-start gap-4">
-            <span className="shrink-0 rounded-lg bg-gray-800 p-4 text-green-400">
-              <svg
-                className="h-5 w-5"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path d="M12 14l9-5-9-5-9 5 9 5z"></path>
-                <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"></path>
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
-                ></path>
-              </svg>
-            </span>
-
-            <div>
-              <h2 className="text-lg font-bold">Lorem, ipsum dolor.</h2>
-
-              <p className="mt-1 text-sm text-gray-300">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
-                cumque tempore est ab possimus quisquam reiciendis tempora
-                animi! Quaerat, saepe?
-              </p>
-            </div>
-          </div>
-
-          <div className="flex items-start gap-4">
-            <span className="shrink-0 rounded-lg bg-gray-800 p-4 text-green-400">
-              <svg
-                className="h-5 w-5"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path d="M12 14l9-5-9-5-9 5 9 5z"></path>
-                <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"></path>
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
-                ></path>
-              </svg>
-            </span>
-
-            <div>
-              <h2 className="text-lg font-bold">Lorem, ipsum dolor.</h2>
-
-              <p className="mt-1 text-sm text-gray-300">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
-                cumque tempore est ab possimus quisquam reiciendis tempora
-                animi! Quaerat, saepe?
-              </p>
-            </div>
+          <div class="hidden lg:flex lg:w-1/2 lg:justify-center pl-14 pb-14 ">
+            <img
+              class="w-[28rem] h-[28rem] flex-shrink-0 object-cover xl:w-[34rem] xl:h-[34rem] rounded-full shadow-2xl shadow-gray-500 animate__animated animate__swing animate__infinite animate__slower"
+              src="https://images.unsplash.com/photo-1581291518857-4e27b48ff24e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+              alt=""
+            />
           </div>
         </div>
       </div>
