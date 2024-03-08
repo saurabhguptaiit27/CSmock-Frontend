e40a3624ef77bf9a6028a4dee3aeae8dfa9522ba
saurabhguptaiit27 <saurabhguptaiit27@gmail.com>
2024-03-08 06:26:40 +0530

    auth context added

---
+++ src/components/Context/AuthProvider.jsx
@@ -0,0 +1,16 @@
+// AuthProvider.js
+import React, { createContext, useState } from "react";
+
+// Create a context
+export const AuthContext = createContext();
+
+// Provider component
+export const AuthProvider = ({ children }) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
+
+  return (
+    <AuthContext.Provider value={{ isLoggedIn, setIsLoggedIn }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
