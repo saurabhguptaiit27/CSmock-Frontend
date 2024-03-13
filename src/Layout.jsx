cab420237a287d39e41e682716e806eefba440b0
saurabhguptaiit27 <saurabhguptaiit27@gmail.com>
2024-03-14 05:28:26 +0530

    Layout code is improved after addind contextAPIs

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,42 +1,32 @@
-import React from "react";
+import React, { useContext } from "react";
 import Header from "./components/Header/Header.jsx";
 import User from "./components/User/User.jsx";
 import Footer from "./components/Footer/Footer.jsx";
 import { Outlet } from "react-router-dom";
 import Divider from "./components/Divider/Divider.jsx";
 import { useState } from "react";
+import { AuthContext } from "./components/Context/AuthProvider.jsx";
+import AvailabilityUI from "./components/AvailabilityUI/AvailabilityUI.jsx";
 
 const Layout = () => {
-  const [selectedButton, setSelectedButton] = useState("User");
-  const [isloggedIn, setIsLoggedIn] = useState(true);
-  const [toggleProfile, setToggleProfile] = useState(true);
+  const { isLoggedIn, userType, isAvailabilityUI } =
+    useContext(AuthContext);
 
-  const handleButtonClick = (button) => {
-    setSelectedButton(button);
-  };
+  const [toggleProfile, setToggleProfile] = useState(false);
   const handleToggleProfile = () => {
     setToggleProfile(!toggleProfile);
-  };
-  const handleLogoutButton = () => {
-    setIsLoggedIn(!isloggedIn);
   };
 
   return (
     <>
       <Header
-        handleButtonClick={handleButtonClick}
         handleToggleProfile={handleToggleProfile}
-        handleLogoutButton={handleLogoutButton}
-        isloggedIn={isloggedIn}
-        setIsLoggedIn={setIsLoggedIn}
       />
-      {toggleProfile && isloggedIn && (
-        <User handleLogoutButton={handleLogoutButton} />
+      {toggleProfile && isLoggedIn && <User />}
+      {isLoggedIn && userType === "Expert" && isAvailabilityUI && (
+        <AvailabilityUI />
       )}
-      <Outlet
-        context={[selectedButton, setSelectedButton, handleButtonClick]}
-      />
-
+      <Outlet />
       <Divider />
       <Footer />
     </>
