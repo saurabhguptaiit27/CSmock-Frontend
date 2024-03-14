4e4b668149f8a2a7afd05363867c176961957090
saurabhguptaiit27 <saurabhguptaiit27@gmail.com>
2024-03-14 05:31:42 +0530

    user jsx profile view and logout button updated

--- src/components/User/User.jsx
+++ src/components/User/User.jsx
@@ -1,55 +1,100 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "../Context/AuthProvider.jsx";
+import { CurrentUserContext } from "../Context/CurrentUserProvider.jsx";
+import { useNavigate } from "react-router-dom";
+import { LiaPowerOffSolid } from "react-icons/lia";
+import { CiSettings } from "react-icons/ci";
+import { PiHandshake } from "react-icons/pi";
+import { HiOutlineUserCircle } from "react-icons/hi2";
 
-const User = ({ handleLogoutButton }) => {
+const User = () => {
+  const { setIsLoggedIn, userType, setUserType } = useContext(AuthContext);
+  const { currentUser } = useContext(CurrentUserContext);
+  const navigate = useNavigate();
+
+  const handleLogoutButton = async () => {
+    try {
+      const response = await fetch(
+        userType === "User"
+          ? "http://localhost:8000/api/v1/users/logout"
+          : "http://localhost:8000/api/v1/experts/logout",
+        {
+          method: "POST",
+          credentials: "include",
+        }
+      );
+
+      console.log("response is-------:", response);
+
+      if (!response.ok) {
+        throw new Error("Failed to logout---///");
+      }
+
+      setIsLoggedIn(false);
+      setUserType("User");
+      console.log("user logged out successfully");
+      navigate("/");
+    } catch (error) {
+      console.error("Logout error ====== : ", error);
+    }
+  };
+
   return (
-    <div class="float-right mt-10">
+    <div class="fixed top-10 right-2 z-30 mt-10">
       <div
-        class="bg-white end-0 z-10  w-55 rounded-md border border-black shadow-lg mr-5 text-left fixed"
+        class="bg-gray-400/95 end-0 w-52 rounded-md border border-green shadow-lg mr-5 text-left fixed"
         role="menu"
       >
-        <div class="p-2">
+        <div class="p-2 ">
           <a
             href="#"
-            class="block rounded-lg px-4 py-2 text-sm text-gray-500 hover:bg-gray-50 hover:text-gray-700"
+            class="block rounded-lg px-4 py-2 text-sm font-bold text-black hover:bg-gray-50 hover:text-gray-700"
             role="menuitem"
           >
-            View on Storefront
+            {currentUser["fullname"]}
+          </a>
+          <hr className="border-amber-400" />
+
+          <a
+            href="#"
+            class="flex w-full items-center gap-2 rounded-lg px-4 py-2 text-sm font-bold text-black hover:bg-gray-50 hover:text-gray-700"
+            role="menuitem"
+          >
+            <HiOutlineUserCircle />
+            Profile
           </a>
 
           <a
             href="#"
-            class="block rounded-lg px-4 py-2 text-sm text-gray-500 hover:bg-gray-50 hover:text-gray-700"
+            class="flex w-full items-center gap-2 rounded-lg px-4 py-2 text-sm font-bold text-black hover:bg-gray-50 hover:text-gray-700"
             role="menuitem"
           >
-            View Warehouse Info
+            <PiHandshake />
+            Your Bookings
           </a>
 
           <a
             href="#"
-            class="block rounded-lg px-4 py-2 text-sm text-gray-500 hover:bg-gray-50 hover:text-gray-700"
+            class="flex w-full items-center gap-2 rounded-lg px-4 py-2 text-sm font-bold text-black hover:bg-gray-50 hover:text-gray-700"
             role="menuitem"
           >
-            Duplicate Product
+            <CiSettings /> Settings
           </a>
 
-          <a
-            href="#"
-            class="block rounded-lg px-4 py-2 text-sm text-gray-500 hover:bg-gray-50 hover:text-gray-700"
-            role="menuitem"
-          >
-            Unpublish Product
-          </a>
-
-          <form method="POST" action="#">
-            <button
-              type="submit"
+          <div>
+            <Link
+              to=""
               onClick={() => handleLogoutButton()}
-              class="flex w-full items-center gap-2 rounded-lg px-4 py-2 text-sm text-red-700 hover:bg-red-50"
+              class="flex w-full items-center gap-2 rounded-lg px-4 py-2 text-sm font-bold text-red-500/70 hover:bg-red-400
+              hover:text-black"
               role="menuitem"
             >
+              <LiaPowerOffSolid className="text-black" />
               Logout
-            </button>
-          </form>
+            </Link>
+          </div>
         </div>
       </div>
     </div>
