fcdea2f1f495ad7d01f478845f48ff7ec0ee89d1
saurabhguptaiit27 <saurabhguptaiit27@gmail.com>
2024-03-19 08:16:03 +0530

    Divider is removed as was unnecessary

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -3,7 +3,6 @@ import Header from "./components/Header/Header.jsx";
 import User from "./components/User/User.jsx";
 import Footer from "./components/Footer/Footer.jsx";
 import { Outlet } from "react-router-dom";
-import Divider from "./components/Divider/Divider.jsx";
 import { AuthContext } from "./components/Context/AuthProvider.jsx";
 import AvailabilityUI from "./components/AvailabilityUI/AvailabilityUI.jsx";
 
@@ -19,7 +18,6 @@ const Layout = () => {
         <AvailabilityUI />
       )}
       <Outlet />
-      <Divider />
       <Footer />
     </>
   );
