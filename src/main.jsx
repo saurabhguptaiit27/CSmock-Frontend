26554afb738ecdec7d7dd32e4946e62b28d64389
saurabhguptaiit27 <saurabhguptaiit27@gmail.com>
2024-05-05 22:27:56 +0530

    WIP-7

--- src/main.jsx
+++ src/main.jsx
@@ -28,16 +28,13 @@ import TopStories from "./news/TopStories.jsx";
 import PostAJob from "./job/PostAJob.jsx";
 import FindJobs from "./job/FindJobs.jsx";
 
-const Loader = <div>loading...</div>;
-
 const App = () => {
   const { isLoggedIn, userType } = useContext(AuthContext);
   // Define routes based on authentication status
   const routes = [
     {
       path: "/",
-      element: <Layout />,
-      loader: Loader,
+      element: <Home />,
       children: [
         { path: "", element: <Home /> },
         { path: "Ourexperts", element: <OurExperts /> },
