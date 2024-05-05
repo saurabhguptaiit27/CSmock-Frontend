cb662614bb738be31102abb5af2e238498ac9ff8
saurabhguptaiit27 <saurabhguptaiit27@gmail.com>
2024-05-05 22:39:29 +0530

    WIP-8

--- src/main.jsx
+++ src/main.jsx
@@ -28,19 +28,24 @@ import TopStories from "./news/TopStories.jsx";
 import PostAJob from "./job/PostAJob.jsx";
 import FindJobs from "./job/FindJobs.jsx";
 
-const LayoutHOC = (comp) => <Layout>{comp}</Layout>;
-
 const App = () => {
   const { isLoggedIn, userType } = useContext(AuthContext);
   // Define routes based on authentication status
   const routes = [
     {
       path: "/",
-      element: (
-        <Layout>
-          <Home />
-        </Layout>
-      ),
+      element: <Home />,
+    },
+    {
+      path: "/about",
+      element: <About />,
+    },
+  ];
+
+  const old_routes = [
+    {
+      path: "/",
+      element: <Layout />,
       children: [
         { path: "", element: <Home /> },
         { path: "Ourexperts", element: <OurExperts /> },
