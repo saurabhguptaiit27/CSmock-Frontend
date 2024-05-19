1f0c1afda1a385400e1dfc2a611ed86588f63e99
saurabhguptaiit27 <saurabhguptaiit27@gmail.com>
2024-05-19 19:06:30 +0530

    WIP-13

--- src/components/Header/Sidebar.jsx
+++ src/components/Header/Sidebar.jsx
@@ -17,7 +17,7 @@ const Sidebar = () => {
           <nav>
             <Link
               className="flex items-center px-4 py-2 text-gray-100 transition-all duration-800 transform rounded-md hover:bg-gray-900  hover:text-gray-100 hover:shadow-md hover:shadow-gray-100/40 duration-300"
-              to=""
+              to="/"
             >
               <FaHome />
 
