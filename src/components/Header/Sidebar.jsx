2fab1ee23626dec7ee52837e8d62ed4626b698e7
saurabhguptaiit27 <saurabhguptaiit27@gmail.com>
2024-06-01 00:21:26 +0530

    sidebar updated

--- src/components/Header/Sidebar.jsx
+++ src/components/Header/Sidebar.jsx
@@ -8,8 +8,10 @@ import { IoNewspaperSharp } from "react-icons/io5";
 import { PiHandshake } from "react-icons/pi";
 import { FaSearchPlus } from "react-icons/fa";
 import { AuthContext } from "../Context/AuthProvider";
+import { CurrentUserContext } from "../Context/CurrentUserProvider";
 const Sidebar = () => {
   const { isLoggedIn } = useContext(AuthContext);
+  const { currentUser } = useContext(CurrentUserContext);
   return (
     <aside className="fixed top-10 leftt-2 z-30 mt-10 ml-2">
       <div className="flex flex-col w-56 h-auto px-2 py-6 overflow-y-auto bg-gray-700/90 rounded-lg z=30">
@@ -82,12 +84,12 @@ const Sidebar = () => {
 
           <a href="#" className="flex items-center px-4 -mx-2">
             <img
-              className="object-cover mx-2 rounded-full h-9 w-9"
-              src="https://images.unsplash.com/photo-1531427186611-ecfd6d936c79?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80"
+              className="object-cover w-7 h-7 rounded-full ring ring-gray-300"
+              src={currentUser.avatar}
               alt="avatar"
             />
             <span className="mx-2 font-medium text-gray-800 dark:text-gray-200">
-              John Doe
+              {currentUser.fullname}
             </span>
           </a>
         </div>
